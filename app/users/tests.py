39f3c74f9d507d4cee4efc4a892334e751b79f0b
900chan <forjung1@gmail.com>
2025-07-01 16:01:59 +0900

    🎨 Day_04 create custom user

--- app/users/tests.py
+++ app/users/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+# TDD : Test Driven Development (테스트 주도 개발)
+
+class UserTestCase(TestCase):
+
+    # 일반 유저 생성 테스트 함수
+    def test_create_user(self):
+        email = 'absbrb@naver.com'
+        password = 'password123'
+
+        user = get_user_model().objects.create_user(email=email, password=password)
+        # 유저가 정상적으로 만들어졌는지 체크
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+        self.assertFalse(user.is_superuser)
+
+
+    # 슈퍼 유저 생성 테스트 함수
+    def test_create_superuser(self):
+        email = 'absbrb_super@naver.com'
+        password = 'password123'
+
+        user = get_user_model().objects.create_superuser(
+            email=email,
+            password=password
+        )
+
+        # 슈퍼유저
+        self.assertTrue(user.is_staff)
+        self.assertTrue(user.is_superuser)
