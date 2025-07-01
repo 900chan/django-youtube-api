8fe1943bf9f2443e64ff0f050e70eaa74a076667
900chan <forjung1@gmail.com>
2025-07-01 18:02:39 +0900

    🎨 Django_Youtube_RestAPI_DAY04
    유저 모델 커스텀, 전체 모델 업데이트, DRF 설정

--- app/common/models.py
+++ app/common/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# - created_at: 데이터 생성시간
+# - updated_at: 데이터 업데이트 시간
+class CommonModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
--- app/videos/models.py
+++ app/videos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from common.models import CommonModel
+from users.models import User
 
-# Create your models here.
+
+class Video(CommonModel):
+    title = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+    link = models.URLField()
+    category = models.CharField(max_length=20)
+    views_count = models.PositiveIntegerField(default=0)
+    thumbnail = models.URLField(blank=True) # S3 Bucket -> Save File -> URL -> Save URL
+    video_file = models.FileField(upload_to='storage/') # upload_to='저장경로'
+
+    # User : Video -> 1:N
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
